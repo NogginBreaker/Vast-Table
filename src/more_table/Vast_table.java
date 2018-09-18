80abba235ff6e170f73f665cdb8b273d295ff16b
zachary.moncur <zachary.moncur@CLC-LAB209-11.TCSD.tooelesd.org>
2018-09-18 13:58:09 -0600

    Table for chapter 2

---
+++ src/more_table/Vast_table.java
@@ -0,0 +1,35 @@
+package more_table;
+
+public class Vast_table {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is Fun");
+		System.out.println("");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("");
+		System.out.println("''Give a man a fire and he'll be warm for a day. Set a man on fire and he'll be warm for the rest of his life''-me");
+		System.out.println("");
+		System.out.println("|______________|");
+		System.out.println("|a   a^2   a^3 |");
+		System.out.println("|~~~~~~~~~~~~~~|");
+		System.out.println("|  1   1    1  |");
+		System.out.println("|  2   4    8  |");
+		System.out.println("|  3   9    27 |");
+		System.out.println("|  4   16   64 |");
+		System.out.println("");
+		System.out.println("a	a^2	a^3\r\n" + 
+				"1	1	1\r\n" + 
+				"2	4	8\r\n" + 
+				"3	9	27\r\n" + 
+				"4	16	64\r\n" + 
+				"");
+	}
+
+}
